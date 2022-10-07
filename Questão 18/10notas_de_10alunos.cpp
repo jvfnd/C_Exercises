aa81ad2357f3dc2736f1d43e42fd93516dc58100
João Vitor <113381115+jvfnd@users.noreply.github.com>
2022-10-07 18:02:14 -0300

    Questão 18 finalizada

--- Questão 18/10notas_de_10alunos.cpp
+++ Questão 18/10notas_de_10alunos.cpp
@@ -2,30 +2,66 @@
 
 int main(){
 	
-int notasdosalunos[10][10], aluno, nota;
-float soma, media, maior, menor;
+int aluno, nota;
+float notasdosalunos[10][10], soma, media, maior, menor;
 
 for(aluno=0; aluno<10; aluno++){
 	for(nota=0; nota<10; nota++){
 		
-		printf("Digite a nota %d do Aluno %d: ", nota, aluno);
-		scanf("%f", &notasdosalunos[aluno][nota]);
-		
-		if((notasdosalunos[nota] < 0) || (notasdosalunos[nota] > 10 )){
-			
-			printf("\n\n\tERRO! DIGITE UM VALOR DE 0 A 10");
 			printf("Digite a nota %d do Aluno %d: ", nota, aluno);
 			scanf("%f", &notasdosalunos[aluno][nota]);
+			
 		}
 	}
-}
+
 for(aluno=0; aluno<10; aluno++){
 	for(nota=0; nota<10; nota++){
 		
-		soma = soma + notasdosalunos[aluno][nota];
+		soma = soma + notasdosalunos[aluno][nota];	
+	} 
 		
-		printf("A soma das Notas do Aluno %d eh: %d", aluno, soma);
-		
+printf("\nA soma das Notas do Aluno %d eh: %.2f", aluno, soma);
+soma = 0;
+
+}
+
+printf("\n");
+maior = 0;
+
+for(aluno=0; aluno<10; aluno++){
+	for(nota=0; nota<10; nota++){
+		if(notasdosalunos[aluno][nota] > maior){
+			maior = notasdosalunos[aluno][nota];
+		}	
 	}
+	printf("\nA Maior nota do Aluno %d eh: %.2f", aluno, maior);
+	maior = 0;
+}
+printf("\n");
+
+for(aluno=0; aluno<10; aluno++){
+	for(nota=0; nota<10; nota++){
+			soma = soma + notasdosalunos[aluno][nota];
+			media = soma / 10;
+	}
+	
+printf("\nA Media das Notas do Aluno %d eh: %.2f", aluno, media);
+media=0;
+soma=0;
+	
+}
+
+menor=10;
+
+printf("\n");
+
+for(aluno=0; aluno<10; aluno++){
+	for(nota=0; nota<10; nota++){
+	if(notasdosalunos[aluno][nota] < menor){
+		menor = notasdosalunos[aluno][nota];	
+	}
+	}
+	printf("\nA Menor nota do Aluno %d eh: %.2f", aluno, menor);
+	menor=10;
 }
 }
